fbcd53ac88b71f8e531f21bba3e5cc5f32352d4a
Legendtech Lord <legendtechlord@gmail.com>
2025-08-03 17:30:10 +0100

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Shared validation function
 function validateCourseId(courseId) {
-    const validCourses = ['crs1', 'crs2'];
+    const validCourses = ['crs1', 'crs2', 'crs3', 'crs4', 'crs5', 'crs6', 'crs7'];
     return validCourses.includes(courseId);
 }
